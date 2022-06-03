1f975ff8f0dd42c346c319c4854192bc62b0ca13
Oli <1024958@student.sae.edu.au>
2022-06-03 23:24:01 +1000

    Add Momentum to Bomb Tilt

--- Assets/Scripts/BombTilt.cs
+++ Assets/Scripts/BombTilt.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BombTilt : MonoBehaviour
 {
     public PlayerController playerController;
-    public float tiltSpeed, maxBombAngle, minBombAngle;
+    public float counterMovementSpeed, tiltSpeed, maxBombAngle, minBombAngle, leftTiltAngle, rightTiltAngle, maxTiltSpeed;
     void Start()
     {
         
@@ -16,18 +16,33 @@ public class BombTilt : MonoBehaviour
     {
         PlayerMovement();
         //Debug.Log("Z Rotation = " + transform.rotation.eulerAngles.z);
+        
+        // right heavy
+        if (transform.eulerAngles.z > minBombAngle && transform.eulerAngles.z < rightTiltAngle)
+        {
+            tiltSpeed = Mathf.Pow(0.5f + ((360 - transform.eulerAngles.z) / maxBombAngle), 2) * maxTiltSpeed;
+            transform.Rotate(0, 0, Vector3.back.z * tiltSpeed * Time.deltaTime);
+        }
+        
+        // left heavy
+        if (transform.eulerAngles.z < maxBombAngle && transform.eulerAngles.z > leftTiltAngle)
+        {
+            tiltSpeed = Mathf.Pow(0.5f + (transform.eulerAngles.z / maxBombAngle), 2) * maxTiltSpeed;
+            transform.Rotate(0, 0, Vector3.forward.z * tiltSpeed * Time.deltaTime);
+        }
 
-        //if (transform.eulerAngles.z > minBombAngle && transform.eulerAngles.z < maxBombAngle)
-        //{
-
-        //}
+        // balanced
+        if (transform.eulerAngles.z < leftTiltAngle || transform.eulerAngles.z > rightTiltAngle)
+        {
+            tiltSpeed = 0f;
+        }
     }
 
     public void PlayerMovement()
     {
         if (Input.GetAxis("Horizontal") != 0)
         {
-            transform.Rotate(0, 0, Input.GetAxisRaw("Horizontal") * tiltSpeed);
+            transform.Rotate(0, 0, Input.GetAxisRaw("Horizontal") * counterMovementSpeed);
         }
 
         if (transform.rotation.eulerAngles.z < minBombAngle && transform.eulerAngles.z > maxBombAngle)
